5cdb0062c001c8aac483c8c397c3d52ec779f147
lidrail35 <lidrail35@mail.ru>
2023-02-02 11:48:49 +0300

    feat: add solve task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+ return (matrix.length===0) ? [] : ((matrix.map((i, x) => (x%2) ? i.reverse() : i)).join()).split(',').map(x=>+x);
 }
+
